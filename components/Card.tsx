99a35acb16d77b17c95a55fcfba20d8fff1ee976
magnuschase <jakub@kapala.xyz>
2022-12-05 11:23:44 +0100

    refactor: add onClick to card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import Link from 'next/link'
+import React, { useCallback } from 'react'
+import { useRouter } from 'next/router'
 
 const Card = ({
 	children,
-	link
+	link,
+	onClick
 }: {
 	children: React.ReactNode,
-	link: string
+	link?: string
+	onClick?: () => void
 }) => {
+	const router = useRouter()
+
+	const handler = useCallback(() => {
+		if (link) {
+			router.push(link)
+			return
+		}
+		if (!onClick) return
+		onClick()
+		return
+	}, [link, onClick, router])
+
 	return (
-		<Link href={link}>
 			<div 
 				className={`
 					p-8 bg-emerald-900 aspect-[2/1] flex items-start justify-start lg:text-5xl
 					rounded-lg shadow-xl shadow-emerald-900 transition-all hover:shadow-emerald-700
-					hover:shadow-2xl hover:bg-emerald-800 text-emerald-500 font-bold text-3xl
+					hover:shadow-2xl hover:bg-emerald-800 text-emerald-500 font-bold text-3xl select-none
+					cursor-pointer
 				`}
+				onClick={handler}
 			>
 				{children}
 			</div>
-		</Link>
 	)
 }
 
