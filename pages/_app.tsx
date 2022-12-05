72c6137e361ee7b579fc3191bfb3a5e0915078e3
magnuschase <jakub@kapala.xyz>
2022-12-05 17:03:19 +0100

    refactor: custom app.tsx file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { AnimatePresence } from 'framer-motion'
+import Nav from '../components/Nav'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+		<>
+			<Head>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<Nav />
+			<AnimatePresence
+				exitBeforeEnter
+				initial={false}
+				onExitComplete={() => window.scrollTo(0, 0)}
+			>
+				<Component {...pageProps} />
+			</AnimatePresence>
+		</>
+	)
 }
